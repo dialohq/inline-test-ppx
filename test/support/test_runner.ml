0bbb7268032c02c7fccdc4bfef5e96a25cfa7279
Paul Tsnobiladzé <paul.tsnobiladze@gmail.com>
2021-12-31 15:55:11 +0100

    fix ci

--- test/support/test_runner.ml
+++
@@ -1,6 +0,0 @@
-(** Main entry point for our test runner.
-
-    This aggregates all the test suites and call Alcotes to run them. When
-    creating a new test suite, don't forget to add it here! *)
-
-(* let () = Alcotest.run "inline-test-ppx" [ "Simple", Simple_test.suite ] *)
